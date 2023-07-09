69090d23de72dc2be291309dcc5755bb28cae5ee
Your Name <larry28chan@protonmail.com>
2023-07-09 15:58:53 +0800

    code completion

--- rps.py
+++ rps.py
@@ -14,6 +14,8 @@ corresponding probabilities
 # Declare Variables
 ROCK, PAPER, SCISSORS, NUM_ACTIONS = 0, 1, 2, 3 
 strategy = np.zeros(NUM_ACTIONS)
+strategySum = np.zeros(NUM_ACTIONS)
+regretSum = np.zeros(NUM_ACTIONS)
 # Opponent uses the mixed strat (R, S, P) = (0.4, 0.3, 0.3)
 oppStrategy = np.array([0.4, 0.3, 0.3])
 
@@ -30,26 +32,25 @@ def getValue(p1, p2):
         return -1
 
 # accmulate in stratSum
-def getStrategy(regretSum):
+def getStrategy():
+    global regretSum, strategySum
     strategy = np.maximum(regretSum, 0)
     normalizingSum = np.sum(strategy)
-
     if normalizingSum > 0:
         strategy /= normalizingSum
     else:
         strategy = np.ones(NUM_ACTIONS)/NUM_ACTIONS
-
+    strategySum += strategy
     return strategy
 
 def getAction(strategy):
     # Returns a random number in (0,1)
-    strategy = strategy / np.sum(strategy)
     rr = random.random()
     # Returns 0, 1, 2 based on the mixed strategy
-    return np.searchsorted(np.cumsum(strategy), rr)
+    return np.searchsorted(np.cumsum(strategy/np.sum(strategy)), rr)
 
 
-def getStrategyPayoff(myStrat,oppStrat, iter1, iter2):
+def getStrategyPayoff(myStrat, oppStrat, iter1, iter2):
     vvv = []
     for i in range(iter1):
         vv = 0 
@@ -61,13 +62,12 @@ def getStrategyPayoff(myStrat,oppStrat, iter1, iter2):
 
     return np.mean(vvv)
 
-def updateRegretSum(myStrat, oppStrat, regretSum, iter):
+def updateRegretSum(myStrat, oppStrat, iter):
     # To train the algorithm and modify the regretSum array
-
+    global regretSum
+    actionPayoff = np.zeros(NUM_ACTIONS)
     for i in range(iter):
-        actionPayoff = np.zeros(NUM_ACTIONS)
         
-        myAction = getAction(myStrat)     
         oppAction = getAction(oppStrat)       
 
        # Compute Action Payoffs: ROCK 0; PAPER 1; SCISSORS 2
@@ -76,14 +76,13 @@ def updateRegretSum(myStrat, oppStrat, regretSum, iter):
         actionPayoff[(oppAction+2)%NUM_ACTIONS] = -1 
 
         regretSum += actionPayoff
-    return regretSum
 
-def getAverageStrategy(regretSum):
-
-    normalizingSum = np.sum(regretSum)
+def getAverageStrategy():
+    global strategySum
+    normalizingSum = np.sum(strategySum)
 
     if normalizingSum > 0:
-        avgStrategy = regretSum / normalizingSum
+        avgStrategy = strategySum / normalizingSum
     else:
         avgStrategy = np.ones(NUM_ACTIONS)/NUM_ACTIONS
 
@@ -92,16 +91,14 @@ def getAverageStrategy(regretSum):
 
 def main():
 
-    regretSum = np.zeros(NUM_ACTIONS)
-
-    myStrat = getStrategy(regretSum) # Use default mixed strat (1/3, 1/3, 1/3)
+    myStrat = getStrategy() # Use default mixed strat (1/3, 1/3, 1/3)
+    print("Using default mixed strategy (1/3, 1/3, 1/3)")
     print(getStrategyPayoff(myStrat, oppStrategy, 100, 100))
- 
-    updatedRegretSum = updateRegretSum(myStrat, oppStrategy, regretSum, 10000) # Modify global variable regretSum
-    print(updatedRegretSum)
-    myNewStrat = getAverageStrategy(updatedRegretSum)
-    print(myNewStrat)
-    print(getStrategyPayoff(myNewStrat, oppStrategy, 100, 100))
 
+    updateRegretSum(myStrat, oppStrategy, 100000) # Modify global variable regretSum
+    
+    myNewStrat = getStrategy()
+    print("Using regret matched mixed strategy after training: ", myNewStrat)
+    print(getStrategyPayoff(myNewStrat, oppStrategy, 100, 100))
 
 main()
---
+++ test.py
@@ -0,0 +1,10 @@
+i = 1
+
+def add():
+    global i
+    for j in range(10):
+        i+=1
+
+
+add()
+print(i)
